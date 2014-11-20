1e52667fb84d4292a793e739abcffa6ec724415e
Kokal <slim_shady121.2@abv.bg>
2014-11-20 16:50:46 +0200

    commit 2

--- Cinema_2/cinema.py
+++ Cinema_2/cinema.py
@@ -5,12 +5,11 @@ from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.sqlite import DATETIME
+import datetime
 
 Base = declarative_base()
 date_type = DATETIME(
-    storage_format="%(day)02d-%(month)02d-%(year)04d")
-time_type = DATETIME(
-    storage_format="%(hour)02d:%(min)02d")
+    storage_format="%(day)02d-%(month)02d-%(year)04d %(hour)02d:%(min)02d")
 
 
 class Movie(Base):
@@ -27,7 +26,6 @@ class Projection(Base):
     movie = relationship("Movie", backref="projections")
     type_movie = Column(String)
     date = Column(date_type)
-    time = Column(time_type)
 
 
 class Reservation(Base):
@@ -45,8 +43,43 @@ engine = create_engine("sqlite:///cinema.db")
 Base.metadata.create_all(engine)
 
 session = Session(bind=engine)
-session.add_all([
-    Movie(name="The Hunger Games: Catching Fire", rating=7.9),
-    Movie(name="Wreck-It Ralph", rating=7.8),
-    Movie(name="Her", rating=8.3)])
+
 session.commit()
+
+
+def is_enought_sits(number_of_sits):
+    free_sits = session.query(Reservation
+                              ).filter(Reservation.projection_id=pr_id).count()
+    if number_of_sits == "exit":
+        return True
+    elif free_sits < number_of_sits:
+        return False
+    else:
+        return True
+
+
+def is_the_sit_is_free(sit):
+    if sit[0] > 10 or sit[0] < 1 or sit[1] > 10 or sit[1] < 1:
+        return False
+
+
+
+def make_reservation():
+    user_name = input("Username: ")
+    pr_id = input("Chose projection ID: ")
+    number_of_sits = input("How many sits you need: ")
+    if is_enought_sits(number_of_sits) is False:
+        while is_enought_sits is False:
+            print("Not enoght sits left, you can buy less sits?")
+            number_of_sits = input("You can exit(tipe: exit) or you can try w\
+                ith less sits: ")
+    if number_of_sits == "exit":
+        return number_of_sits
+    sits = [[]]
+    for index in range(number_of_sits):
+        while is_the_sit_is_free(sits[index]) is False:
+            sits[index].append(input("Chose row: "))
+            sits[index].append(input("Chose sit: "))
+            if is_the_sit_is_free[index] is False:
+                print("This sit is already taken or invalid!!! \
+                       Try again!")
